5b3f916d5a1808850c872f4ea1afc07b76e3ee5a
Cyril Jazra <>
2022-06-16 20:17:42 -0700

    Add vae training

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,23 @@
 from .vanilla_vae import *
 from .base import *
+vae_models = {'HVAE':HVAE,
+              'LVAE':LVAE,
+              'IWAE':IWAE,
+              'SWAE':SWAE,
+              'MIWAE':MIWAE,
+              'VQVAE':VQVAE,
+              'DFCVAE':DFCVAE,
+              'DIPVAE':DIPVAE,
+              'BetaVAE':BetaVAE,
+              'InfoVAE':InfoVAE,
+              'WAE_MMD':WAE_MMD,
+              'VampVAE': VampVAE,
+              'GammaVAE':GammaVAE,
+              'MSSIMVAE':MSSIMVAE,
+              'JointVAE':JointVAE,
+              'BetaTCVAE':BetaTCVAE,
+              'FactorVAE':FactorVAE,
+              'LogCoshVAE':LogCoshVAE,
+              'VanillaVAE':VanillaVAE,
+              'ConditionalVAE':ConditionalVAE,
+              'CategoricalVAE':CategoricalVAE}
