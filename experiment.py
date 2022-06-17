261bf14f84d47d5dc0a7d3130a09203bd9888970
Cyril Jazra <>
2022-06-16 20:14:18 -0700

    Add vae training

--- experiment.py
+++ experiment.py
@@ -11,6 +11,21 @@ import torchvision.utils as vutils
 from torchvision.datasets import CelebA
 from torch.utils.data import DataLoader
 
+def data_loader(fn):
+    """
+    Decorator to handle the deprecation of data_loader from 0.7
+    :param fn: User defined data loader function
+    :return: A wrapper for the data_loader function
+    """
+
+    def func_wrapper(self):
+        try: # Works for version 0.6.0
+            return pl.data_loader(fn)(self)
+
+        except: # Works for version > 0.6.0
+            return fn(self)
+
+    return func_wrapper
 
 class VAEXperiment(pl.LightningModule):
 
